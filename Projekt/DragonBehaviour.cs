5e0a8cb65ea01ccae508adc1d4b41097ca1c1c80
Jannfreiwald <jann.freiwald@googlemail.com>
2015-12-16 14:51:58 +0100

    Drachenscript tests

---
+++ Projekt/DragonBehaviour.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonBehaviour : MonoBehaviour {
+    
+    public float speed = 10;
+    public float maxSpeed = 30;
+    float vertical = 0f;
+    float horizontal = 0f;
+    float tilt;
+	public float acceleration = 50f;
+	private float rotateHSpeed ;
+	private float rotateVSpeed ;
+	public float rotateHStart = 0.01f;
+	public float rotateVStart = 0.01f;
+	 CubeScript bodyMovement;
+
+	void Start ()
+    {
+		bodyMovement = GetComponent<CubeScript> ();
+	}
+	
+    void Update()
+    {
+		GetInputSpeed();
+        GetInputVertical();
+        GetInputHorizontal();
+		rotateHSpeed = rotateHStart + ( speed * 0.0001f);
+		rotateVSpeed = rotateVStart + ( speed * 0.0001f);
+    }
+
+	void FixedUpdate ()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        
+        transform.eulerAngles = (new Vector3(-vertical * 90, -horizontal * 90, tilt * 90));
+	}
+
+    void GetInputSpeed()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || bodyMovement.shakeHands())
+        {
+            speed += acceleration;
+        }
+        if (Input.GetKey(KeyCode.S) || bodyMovement.pull())
+        {
+         //   speed -= acceleration/15;
+        }
+        speed = Mathf.Clamp(speed, 0f, maxSpeed);
+    }
+
+    void GetInputVertical()
+    {
+        if (Input.GetKey(KeyCode.DownArrow) || bodyMovement.leanForward())
+        {
+            vertical += rotateVSpeed;
+        }
+		else if (Input.GetKey(KeyCode.UpArrow) || bodyMovement.leanBackward())
+        {
+            vertical -= rotateVSpeed;
+        }
+        else
+        {
+			if(vertical > .1) { vertical -= rotateVSpeed; }
+			else if (vertical < 0) { vertical += rotateVSpeed; }
+        }
+        vertical = Mathf.Clamp(vertical, -1f, 1f);
+    }
+
+    void GetInputHorizontal()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) || bodyMovement.leanLeft())
+        {
+			horizontal += rotateHSpeed;
+			tilt+= rotateHSpeed;
+        }
+		else if (Input.GetKey(KeyCode.RightArrow) || bodyMovement.leanRight())
+        {
+			horizontal -= rotateHSpeed;
+			tilt -= rotateHSpeed;
+        }
+        else 
+		{
+			if(tilt > .1) { tilt -= rotateHSpeed; }
+			else if (tilt < 0) { tilt += rotateHSpeed; }
+        }
+        tilt = Mathf.Clamp(tilt, -1f, 1f);
+    }
+}
---
+++ Projekt/DragonFly.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonFly : MonoBehaviour {
+
+	float startTime;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		transform.position += transform.forward * 10.0f*Time.deltaTime;
+		transform.Rotate(-Input.GetAxis("Vertical")+(Input.GetAxis("Horizontal") ),0.0f,-Input.GetAxis("Horizontal"));
+		if (Input.GetAxis ("Horizontal") == 0) {
+			 startTime = Time.time;
+		}
+		float t = Time.time - startTime;
+		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler(transform.rotation.x, transform.rotation.y,0), t);
+
+	}
+}
